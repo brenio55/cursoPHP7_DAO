f08db1ac74f5fc72a39465d7ca09ae0bbb82fd4e
Brenio Filho <brenio55@outlook.com>
2022-10-27 19:06:07 -0300

    Aula 69 - PDO DAO DELETE

--- class/usuario.php
+++ class/usuario.php
@@ -111,6 +111,18 @@ class Usuario {
         ));
     }
 
+    public function delete(){
+        $sql = new Sql();
+        $sql->runQuery("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdUsuario()
+        ));
+
+        $this->setIdUsuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+    }
+
     public function __construct($login = "", $password = ""){
         $this->setDeslogin($login);
         $this->setDessenha($password);
--- index.php
+++ index.php
@@ -13,29 +13,36 @@
 //     echo $root;
 
     //carrega uma lista de usuários
-    // $lista = Usuario::getList();
-    // echo json_encode($lista);
+        // $lista = Usuario::getList();
+        // echo json_encode($lista);
 
     //carrega uma lista de usuários buscando pelo login
-    // $search = Usuario::search('jo');
-    // echo json_encode($search);
+        // $search = Usuario::search('jo');
+        // echo json_encode($search);
 
     //carrega um usuário usando o login e uma senha
-    // $usuario = new Usuario();
-    // $usuario->login('root', '!@#$');
+        // $usuario = new Usuario();
+        // $usuario->login('root', '!@#$');
 
     //novo usuário
-    // $aluno = new Usuario();
+        // $aluno = new Usuario();
 
-    // $aluno->setDeslogin("aluno");
-    // $aluno->setDessenha("@alun0");
+        // $aluno->setDeslogin("aluno");
+        // $aluno->setDessenha("@alun0");
 
     // $aluno->insert();
 
     //fazendo update no usuario
-    $usuario = new Usuario();
-    $usuario->loadById(8);
-    $usuario->update("profesosr", "!@#$!@#$");
+        // $usuario = new Usuario();
+        // $usuario->loadById(8);
+        // $usuario->update("profesosr", "!@#$!@#$");
 
-    echo $usuario;
+        // echo $usuario;
+
+    //deletando um usuário
+        $usuario = new Usuario();
+        $usuario->loadById(8);
+        $usuario->delete();
+
+        echo $usuario;
 ?>
